f56dc4c5dd290be264066a71de84630e4ebddee5
Chelsea Troy <chelsea.dommert@gmail.com>
2019-12-17 17:02:00 -0600

    Server can send and receive messages to ask about log length, tell its peers its log length, compare a peer's log length to its own, and catch up a peer if it has more logs than a peer

--- server.py
+++ server.py
@@ -3,13 +3,13 @@ import threading
 from key_value_store import KeyValueStore
 from message_pass import *
 from config import server_nodes
+import ast
 
 class Server:
     def __init__(self, name, port=10000):
         self.port = port
         self.name = name
         self.kvs = KeyValueStore()
-        self.catch_up(self.kvs)
 
     def destination_addresses(self):
         other_servers = {k: v for (k, v) in server_nodes().items() if k != self.name}
@@ -23,7 +23,6 @@ class Server:
         f.close()
 
         print("starting up on " + str(server_address[0]) + " port "  + str(server_address[1]))
-        print(self.destination_addresses())
 
         sock = socket()
         sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -48,7 +47,25 @@ class Server:
                         string_operation = operation.decode("utf-8")
                         print("received " + string_operation)
 
-                        response = kvs.execute(string_operation)
+                        if string_operation == "log_length?":
+                            response = "log_length " + str(len(self.kvs.log))
+                        elif string_operation.split(" ")[0] == "log_length":
+                            catch_up_start_index = int(string_operation.split(" ")[1])
+
+                            if len(self.kvs.log) > catch_up_start_index:
+                                response = "catch_up_logs " + str(self.kvs.log[catch_up_start_index:])
+                            else:
+                                response = "Your info is as good as mine!"
+                        elif string_operation.split(" ")[0] == "catch_up_logs":
+                            logs_to_append = ast.literal_eval(string_operation.split("catch_up_logs ")[1])
+                            [self.kvs.execute(log) for log in logs_to_append]
+
+                            response = "Caught up. Thanks!"
+                        elif string_operation == "show_log":
+                            response = str(self.kvs.log)
+                        else:
+                            response = kvs.execute(string_operation)
+
                         send_message(connection, response.encode('utf-8'))
 
                     else:
@@ -57,14 +74,3 @@ class Server:
 
             finally:
                 connection.close()
-
-    #
-    # def catch_up(self, key_value_store):
-    #     f = open("commands.txt", "r")
-    #     log = f.read()
-    #     f.close()
-    #
-    #     for command in log.split('\n'):
-    #         key_value_store.execute(command)
-
-
