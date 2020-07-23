819b13d038d5b025a474df99cea36a72fc32dad6
Chelsea Troy <chelsea.dommert@gmail.com>
2020-07-22 22:00:31 -0500

    Whether a server is the leader was always evaluating to true. Fixed it.

--- start_server.py
+++ start_server.py
@@ -2,4 +2,8 @@ import sys
 
 from src.server import Server
 
-Server(name=str(sys.argv[1]), port=int(sys.argv[2]), leader=bool(sys.argv[3])).start()
+am_i_the_leader = False
+if len(sys.argv) > 3 and sys.argv[3] == 'True':
+    am_i_the_leader = True
+
+Server(name=str(sys.argv[1]), port=int(sys.argv[2]), leader=am_i_the_leader).start()
